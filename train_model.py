a81fb87e09fe1b62b252eac153c9e7a49c59758e
Nikhil B <bn2039v@gmail.com>
2026-02-24 14:45:58 +0530

    Switch to Keras format model

--- train_model.py
+++ train_model.py
@@ -1,12 +1,17 @@
 import tensorflow as tf
 import numpy as np
+import os
 from tensorflow.keras.preprocessing import image
 
-# Load trained model
-model = tf.keras.models.load_model("model/deepfake_model.h5")
+# 🔥 Use absolute safe path
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "deepfake_model.keras")
+
+# Load trained model (NEW FORMAT)
+model = tf.keras.models.load_model(MODEL_PATH)
 
 def predict_image(img_path):
-    img = image.load_img(img_path, target_size=(224,224))
+    img = image.load_img(img_path, target_size=(224, 224))
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0) / 255.0
 
@@ -17,4 +22,4 @@ def predict_image(img_path):
     if prediction > 0.5:
         return "FAKE", confidence
     else:
-        return "REAL", 100 - confidence
+        return "REAL", round(100 - confidence, 2)
